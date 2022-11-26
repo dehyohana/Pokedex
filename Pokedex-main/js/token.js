b986481657c8bc783a8c5eb62e1a652f6f20a35f
PokedexBCC <115326338+PokedexBCC@users.noreply.github.com>
2022-11-25 21:15:21 -0300

    Delete Pokedex-main directory

--- Pokedex-main/js/token.js
+++
@@ -1,9 +0,0 @@
-if(sessionStorage.getItem('token') == null){
-    alert('Você precisa estar logado para acessar esssa página')
-    window.location.href = '/login.html'
-}
-
-function sair(){
-    sessionStorage.removeItem('token')
-    window.location.href = '/login.html'
-}
